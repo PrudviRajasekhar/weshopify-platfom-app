43adf4fbaaab21002df2818f90fb546f1c2a0def
Narsi <narsi.myteaching@gmail.com>
2022-05-10 08:24:56 +0530

    implemented customer dashboard rendering

--- we-shopify-platform/src/main/java/com/weshopify/platform/features/customers/CustomerBean.java
+++ we-shopify-platform/src/main/java/com/weshopify/platform/features/customers/CustomerBean.java
@@ -9,6 +9,13 @@ public class CustomerBean implements Serializable {
 	 */
 	private static final long serialVersionUID = -3392255258797133035L;
 	
+	private int customerId;
+	public int getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
 	private String userName;
 	private String email;
 	private String password;
@@ -41,6 +48,50 @@ public class CustomerBean implements Serializable {
 	}
 	public void setMobileNumber(String mobileNumber) {
 		this.mobileNumber = mobileNumber;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + customerId;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((mobileNumber == null) ? 0 : mobileNumber.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((userName == null) ? 0 : userName.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CustomerBean other = (CustomerBean) obj;
+		if (customerId != other.customerId)
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (mobileNumber == null) {
+			if (other.mobileNumber != null)
+				return false;
+		} else if (!mobileNumber.equals(other.mobileNumber))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (userName == null) {
+			if (other.userName != null)
+				return false;
+		} else if (!userName.equals(other.userName))
+			return false;
+		return true;
 	}
 
 }
--- we-shopify-platform/src/main/java/com/weshopify/platform/features/customers/CustomerController.java
+++ we-shopify-platform/src/main/java/com/weshopify/platform/features/customers/CustomerController.java
@@ -1,39 +1,47 @@
 package com.weshopify.platform.features.customers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import com.weshopify.platform.features.customers.service.CustomerService;
 
 @Controller
 public class CustomerController {
 
-	@RequestMapping(value = {"/customer"},method = RequestMethod.GET)
+	Logger log = LoggerFactory.getLogger(CustomerController.class);
+
+	@Autowired
+	private CustomerService customerService;
+
+	@RequestMapping(value = { "/customer" }, method = RequestMethod.GET)
 	public String renderSignupPage() {
+		log.info("in render signup page method");
 		return "sign-up.html";
 	}
 	
-	/*
-	 * @RequestMapping(value = {"/customer"},method = RequestMethod.POST) public
-	 * String createCustomer(@RequestParam String userName,@RequestParam String
-	 * email,
-	 * 
-	 * @RequestParam String mobileNumber,@RequestParam String password) {
-	 * System.out.println("Customer Registration information is:\t");
-	 * System.out.println(userName); System.out.println(email);
-	 * System.out.println(mobileNumber); System.out.println(password); return
-	 * "sign-up.html"; }
-	 */
-	
-	@RequestMapping(value = {"/customer"},method = RequestMethod.POST)
-	public String createCustomer(CustomerBean customer) {
-		System.out.println("Customer Registration information is:\t");
-		System.out.println(customer.toString());
+	@RequestMapping(value = { "/view-customers" }, method = RequestMethod.GET)
+	public String viewCustomersPage() {
+		log.info("in render signup page method");
+		return "customer.html";
+	}
+
+
+	@RequestMapping(value = { "/customer" }, method = RequestMethod.POST)
+	public String createCustomer(CustomerBean customer, Model model) {
+		log.info("Customer Registration information is:\t");
+		log.info(customer.toString());
+		customer = customerService.saveCustomer(customer);
+		if (customer.getCustomerId() > 0) {
+
+			String message = "User Registration Successfull. Proceed with the Login Login";
+			model.addAttribute("message", message);
+		}
 		return "sign-up.html";
 	}
-	
-	
-	
-	
-	
+
 }
