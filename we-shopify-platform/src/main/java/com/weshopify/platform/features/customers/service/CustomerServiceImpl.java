bb5001de2338e71c4c4887d90022b3b1fab6a326
Narsi <narsi.myteaching@gmail.com>
2022-05-26 08:11:56 +0530

    integreated the customer repo with the customer service

--- we-shopify-platform/src/main/java/com/weshopify/platform/features/customers/service/CustomerServiceImpl.java
+++ we-shopify-platform/src/main/java/com/weshopify/platform/features/customers/service/CustomerServiceImpl.java
@@ -5,14 +5,19 @@ package com.weshopify.platform.features.customers.service;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
 import com.weshopify.platform.features.customers.CustomerBean;
+import com.weshopify.platform.features.customers.models.Customer;
+import com.weshopify.platform.features.customers.repository.CustomerDataRepo;
 
 /**
  * @author IM-LP-1763
@@ -23,7 +28,14 @@ public class CustomerServiceImpl implements CustomerService {
 	
 	//private static Set<CustomerBean> IN_MEMORY_DB = new HashSet<CustomerBean>();
 	private static Map<Integer, CustomerBean> IN_MEMORY_DB = new HashMap<Integer, CustomerBean>();
-
+	
+	
+	private CustomerDataRepo customerRepo;
+     
+	@Autowired
+	public CustomerServiceImpl(CustomerDataRepo customerRepo) {
+		this.customerRepo=customerRepo;
+	}
 	/**
 	 * if the id is there in the customer bean
 	 * then update the database with the data that is coming inside
@@ -32,40 +44,63 @@ public class CustomerServiceImpl implements CustomerService {
 	@Override
 	public CustomerBean saveCustomer(CustomerBean customerBean) {
 		
-		if(customerBean.getCustomerId() != 0) {
-			IN_MEMORY_DB.put(customerBean.getCustomerId(),customerBean);
-		}else {
-			int customerId = new Random().nextInt();
-			customerBean.setCustomerId(customerId);
-			IN_MEMORY_DB.put(customerId,customerBean);
-		}
+		/*
+		 * if(customerBean.getCustomerId() != 0) {
+		 * IN_MEMORY_DB.put(customerBean.getCustomerId(),customerBean); }else { int
+		 * customerId = new Random().nextInt(); customerBean.setCustomerId(customerId);
+		 * IN_MEMORY_DB.put(customerId,customerBean); }
+		 */
+		/**
+		 * Step-1: Convert the bean data to domain to save in database
+		 */
+		Customer customerDomain = new Customer();
+		BeanUtils.copyProperties(customerBean, customerDomain);
+		customerRepo.save(customerDomain);
 		
+		/**
+		 * Step-2: Convert the domain data to bean again beacuse domain might have 
+		 * the auto generated ID to set it back to the bean to send it again for the 
+		 * futher CRD operations
+		 */
+		BeanUtils.copyProperties(customerDomain, customerBean);
 		return customerBean;
 	}
 
 	@Override
 	public CustomerBean updateCustomer(CustomerBean customerBean) {
-		IN_MEMORY_DB.put(customerBean.getCustomerId(),customerBean);
-		return customerBean;
+		//IN_MEMORY_DB.put(customerBean.getCustomerId(),customerBean);
+		return saveCustomer(customerBean);
 	}
 
 	@Override
 	public List<CustomerBean> findAllCustomers() {
 		// TODO Auto-generated method stub
-		List<CustomerBean> customerList = new ArrayList<CustomerBean>(IN_MEMORY_DB.values());
+		List<CustomerBean> customerList = new ArrayList<>();
+		Iterable<Customer> customerIterator = customerRepo.findAll();
+		
+		customerIterator.forEach(customer->{
+			CustomerBean customerBean = new CustomerBean();
+			BeanUtils.copyProperties(customer, customerBean);
+			customerList.add(customerBean);
+			
+		});
 		return customerList;
 	}
 
 	@Override
 	public CustomerBean findCustomerById(int customerId) {
-		return IN_MEMORY_DB.get(customerId);
+		Customer customer = customerRepo.findById(customerId).get();
+		CustomerBean customerBean = new CustomerBean();
+		BeanUtils.copyProperties(customer, customerBean);
+		return customerBean;
 	}
 
 	@Override
 	public List<CustomerBean> deleteCustomer(int customerId) {
 		IN_MEMORY_DB.remove(customerId);
-		List<CustomerBean> customerList = new ArrayList<CustomerBean>(IN_MEMORY_DB.values());
-		return customerList;
+		customerRepo.deleteById(customerId);
+		//List<CustomerBean> customerList = new ArrayList<CustomerBean>(IN_MEMORY_DB.values());
+		return findAllCustomers();
 	}
 
 	@Override
